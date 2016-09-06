ae81c3549928bbe225cb2f20a8a087f00d1ed40c
Vadik76 <Vgetsov76@gmail.com>
2016-09-06 18:38:56 -0400

    9/1

---
+++ class07/first-javascript/script.js
@@ -0,0 +1,53 @@
+alert("Hi, my name is: Vadim");
+
+document.write("this is something")
+
+document.write('<br>');
+
+document.write(10 * 10);
+
+document.write('<br>');
+
+document.write(100 / 10);
+
+document.write('<br>');
+
+document.write(50 / 17);
+
+document.write('<br>');
+
+document.write(50 + 17);
+
+document.write('<br>');
+
+var num1 = 10 * 10;
+
+document.write('<br>');
+
+document.write(num1);
+
+console.log(num1);
+
+var num2 = 55;
+
+var num3;
+
+console.log("num3: " + num3);
+
+function doSomething() {
+	console.log('this is working');
+}
+
+// call the function
+doSomething();
+
+
+
+
+
+
+
+
+
+
+
