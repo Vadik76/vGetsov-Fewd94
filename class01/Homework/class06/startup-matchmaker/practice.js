b939675a28eb610de04006e2a4daa9f2970cb730
Vadik76 <Vgetsov76@gmail.com>
2016-09-13 18:35:23 -0400

    a

--- class01/Homework/class06/startup-matchmaker/practice.js
+++ class01/Homework/class06/startup-matchmaker/practice.js
@@ -1,1 +1,14 @@
-var age = document.getElementByTag("input").value
+var age = document.getElementByTag("input")
+
+var age = document.getElementById
+
+age = "30"
+
+parseInt("30")
+
+30
+
+
+if(30>21){
+
+}
