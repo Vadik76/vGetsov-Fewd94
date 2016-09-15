7a382d5c07ac0a46bd204b1a3d65e6a334530984
Vadik76 <Vgetsov76@gmail.com>
2016-09-14 20:41:29 -0400

    9/12

---
+++ class01/Homework/class10/jquery-review/js/script.js
@@ -0,0 +1,26 @@
+$( document ).ready(function() {
+    console.log( "ready!" );
+
+
+$('.text').css("background-color", "yellow");
+
+$('.text').click(function(){
+	$('.text').css("background-color", "blue");
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
+
