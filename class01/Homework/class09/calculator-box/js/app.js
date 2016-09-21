1d8f4b21bbb817a0ca2a39de59dc63b6d6c6b3df
Vadik76 <Vgetsov76@gmail.com>
2016-09-21 18:36:10 -0400

    hwClass9

--- class01/Homework/class09/calculator-box/js/app.js
+++ class01/Homework/class09/calculator-box/js/app.js
@@ -12,22 +12,60 @@
 // - . text() or .html()
 // - .css()
 $(document).ready(function(){
+
+	console.log("ready")
+
+	var mainthing = $('#main-article-comment-section')
+
 	var total = 0;
 
-	$('#a10').click(function(){
+	var whatever = $('#a10')
+
+	$(whatever).click(function(){
+		console.log("click")
 		total = total + 10;
-		$('#out');html(total);
+		$('#out').html(total);
 	});
 
 	$('#a20').click(function(){
 		total = total + 20;
-		$('#out');html(total);
+		$('#out').html(total);
 	});
 
 	$('#a30').click(function(){
 		total = total + 30;
-		$('#out');html(total);
+		$('#out').html(total);
+	});
+
+	$('#n10').click(function(){
+		console.log("click")
+		total = total - 10;
+		$('#out').html(total);
+	});
+
+	$('#n20').click(function(){
+		total = total - 20;
+		$('#out').html(total);
+	});
+
+	$('#n30').click(function(){
+		total = total - 30;
+		$('#out').html(total);
 	});
 
 
-}
+	$('#red').click(function(){
+		$('#out').css('background-color','red')
+	});
+
+	$('#blue').click(function(){
+		$('#out').css('background-color','blue')
+	});
+
+	$('#out').click(function(){
+		total = 0;
+		$('#out').html(total);
+		$('#out').css('background-color','white')
+
+	});
+})
--- class01/Homework/class10/jquery-review/js/script.js
+++ class01/Homework/class10/jquery-review/js/script.js
@@ -12,22 +12,17 @@ $('.box1').click(function(){
 
 $('.box1').hover(function(){
 	$('.box1').css("background-color", "pink");
-})
-
-$('.content').click(function(){
-
-})
-$('.content').html('<p>this is a paragraph</p>')
-
-
-
-
-
-
-
-
-
-
-
 });
+
+
+
+
+
+
+
+
+
+
+
+
 
